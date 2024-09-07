bf06446fc5ef04064d800591096a5bd4fea386e3
kelly_maass <kmaass77627@gmail.com>
2024-09-06 21:08:31 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,23 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    
+    if params[:name]
+      
+     found = students.find {|student| student.first_name.downcase == params[:name] || student.last_name.downcase == params[:name]}
+      # byebug
+       render json: found
+    else
+    
     render json: students
+  end
+
+end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+
   end
 
 end
